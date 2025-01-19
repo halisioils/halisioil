345b9f58926d384011697b3094df34d1a5c1d8e3
Babatunde Taiwo <babatundetaiwo038@gmail.com>
2025-01-19 19:43:06 +0000

    feat: stripe webhook added

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -3,7 +3,7 @@ import Stripe from "stripe";
 import { api } from "~/trpc/server";
 
 // Initialize Stripe with your secret key
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "");
+const stripe = new Stripe(process.env.STRIPE_WEBHOOK_SECRET ?? "");
 
 export async function POST(req: NextRequest) {
   const sig = req.headers.get("stripe-signature") ?? "";
