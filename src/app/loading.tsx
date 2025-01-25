f4a2bf0461f9c8c3a96253edca93537b83f6d65e
Babatunde Taiwo <babatundetaiwo038@gmail.com>
2025-01-25 23:22:09 +0000

    style: product card refix

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
+import LoadingComponent from "~/utils/LoadingComponent";
 import React from "react";
 
 const Loading = () => {
   return (
     <section className="relative mx-auto flex h-full min-h-[70vh] w-[100%] max-w-[1100px] justify-center pt-[2rem]">
-      <div className="mb-2.5 flex h-[600px] w-[100%] items-center justify-between rounded-[15px] bg-gray-200 dark:bg-gray-700"></div>
+      <LoadingComponent />
     </section>
   );
 };
